2bec0b56439167def21f648bab8fa00b41936cea
Danny Pesses <dcpesses@users.noreply.github.com>
2024-06-17 12:27:26 -0700

    feat: add handling for multiple container/row/tile types, add row headers, fix row navigation ux

--- src/script.js
+++ src/script.js
@@ -1,38 +1,67 @@
 import { fetchHomePage } from './api';
-import { createTile } from './tile';
+import { createTile, RowTypes } from './tile';
 import { createModal } from './modal';
 import './styles.css';
 
 document.addEventListener('DOMContentLoaded', () => {
     const app = document.getElementById('app');
-    // const modal = document.getElementById('modal');
-    // const modalContent = document.getElementById('modal-content');
-    const {modal, modalContent} = createModal();
 
-    modal.appendChild(modalContent);
+    const {modal, modalContent} = createModal();
 
     let rows = [];
     let tiles = [];
     let focusedIndex = 0;
 
+    function createRowHeader(rowDataSet) {
+        let text = rowDataSet?.text?.title?.full?.set?.default?.content;
+        if (!text) {
+            text = '';
+        }
+        const headerElement = document.createElement('h3');
+        headerElement.classList.add('row-header');
+        headerElement.textContent = text;
+        return headerElement;
+    }
     function renderHomePage(data) {
         console.log({data});
         const {containers} = data.data.StandardCollection;
         let rowIdx = 0;
-        containers.forEach(rowData => {
+        let tileIdx = 0;
+        let rowType = RowTypes.GRID;
+        containers.forEach((rowData, containerIdx) => {
             try {
                 if (!rowData.set?.items) {
                     console.warn('No items found in set:', rowData.set);
                     return;
                 }
+                switch (containerIdx) {
+                    case 0:
+                        rowType = RowTypes.BILLBOARD;
+                        break;
+                    case 1:
+                        rowType = RowTypes.COLLECTION;
+                        break;
+                    default:
+                        rowType = RowTypes.GRID;;
+                        break;
+                }
+
                 const rowElement = document.createElement('div');
+                rowElement.id = rowData.set.setId;
                 rowElement.classList.add('row');
                 rowElement.dataset.rowIdx = rowIdx;
-                rowData.set.items.forEach((item, rowIdx) => {
-                    const tile = createTile(item);
+                if (rowType !== RowTypes.BILLBOARD) {
+                    const rowHeader = createRowHeader(rowData.set);
+                    rowElement.appendChild(rowHeader);
+                }
+                rowData.set.items.forEach((item) => {
+                    const tile = createTile(item, rowType);
                     tile.dataset.rowIdx = rowIdx;
+                    tile.dataset.tileIdx = tileIdx;
                     rowElement.appendChild(tile);
                     tiles.push(tile);
+                    tileIdx += 1;
+                    console.log({tiles: tiles.length, tileIdx});
                 });
                 rows.push(rowElement);
                 app.appendChild(rowElement);
@@ -48,31 +77,25 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function focusTile(index) {
+        console.log({index, focusTile: tiles[index]});
         tiles[focusedIndex].classList.remove('focused');
         focusedIndex = index;
         tiles[focusedIndex].classList.add('focused');
         tiles[focusedIndex].scrollIntoView({
             behavior: 'smooth',
             block: 'nearest',
-            inline: 'nearest',
-            // inline: 'center'
+            // inline: 'nearest',
+            // overflow: 'clip',
         });
     }
 
     function focusRow(index) {
-        let focusedRowIndex = index;
-        // tiles[focusedIndex].classList.remove('focused');
-        // focusedIndex = index;
-        // tiles[focusedIndex].classList.add('focused');
-        // tiles[focusedIndex].scrollIntoView({
-        //     behavior: 'smooth',
-        //     block: 'nearest',
-        //     inline: 'nearest',
-        //     // inline: 'center'
-        // });
+        console.log({index});
 
-        console.log('rows[focusedRowIndex]:', rows[focusedRowIndex]);
-        const nextTile = rows[focusedRowIndex].firstChild();
+        console.log({focusedRow: rows[index]});
+        let nextTile = rows[index].firstChild.nextSibling;
+        console.log({nextTile});
+        nextTile = parseInt(nextTile.dataset.tileIdx, 10);
         focusTile(nextTile);
     }
 
@@ -104,20 +127,23 @@ document.addEventListener('DOMContentLoaded', () => {
                 }
                 break;
             case 'ArrowDown':
-                focusedRow = tiles[focusedIndex].dataset.rowIdx;
+                focusedRow = parseInt(tiles[focusedIndex].dataset.rowIdx, 10);
+                console.log({
+                    focusedTile: tiles[focusedIndex],
+                    dataset: tiles[focusedIndex].dataset,
+                    focusedRow: focusedRow,
+                    rows
+                });
                 if (focusedRow < rows.length - 1) {
-                    focusRow(focusedRow - 1);
-                }
-                // const nextRow = Math.min(tiles.length - 1, focusedIndex + 5);
-                // focusTile(nextRow);
-                break;
-            case 'ArrowUp':
-                focusedRow = tiles[focusedIndex].dataset.rowIdx;
-                if (focusedRow > 0) {
                     focusRow(focusedRow + 1);
                 }
-                // const prevRow = Math.max(0, focusedIndex - 5);
-                // focusTile(prevRow);
+                break;
+            case 'ArrowUp':
+                // focusedRow = tiles[focusedIndex].dataset.rowIdx;
+                focusedRow = parseInt(tiles[focusedIndex].dataset.rowIdx, 10);
+                if (focusedRow > 0) {
+                    focusRow(focusedRow - 1);
+                }
                 break;
             case 'Enter':
                 showTileDetails();
--- src/tile/index.js
+++ src/tile/index.js
@@ -1,42 +1,88 @@
+export const RowTypes = {
+    BILLBOARD: 'billboard',
+    COLLECTION: 'collection',
+    GRID: 'grid'
+};
 
-import { showModal } from '../modal';
-
-export function createTile(item) {
+export function createTile(item, rowType) {
     const tile = document.createElement('div');
     tile.classList.add('tile');
+    if (rowType) {
+        tile.classList.add(rowType);
+    }
     let contentType;
     if (item.seriesId) {
         contentType = 'series';
+        tile.classList.add(`series-${item.seriesId}`);
         // title = item.text.title.full.series.default.content;
     } else if (item.programId) {
         contentType = 'program';
+        tile.classList.add(`program-${item.programId}`);
         // title = item.text.title.full.program.default.content;
     } else if (item.collectionId) {
         contentType = 'collection';
+        tile.classList.add(`collection-${item.collectionId}`);
         // title = item.text.title.full.collection.default.content;
     }
     let title = item.text.title.full[contentType].default.content;
     tile.dataset.title = title;
-    const img = createTileImage({contentType, item, title});
+    const img = createTileImage({contentType, item, rowType, title});
     tile.appendChild(img);
-
-    // tile.addEventListener('click', () => showModal(item));
+    if (rowType === RowTypes.BILLBOARD) {
+        const imgTextTreatment = createTileImage({contentType, isTitleTreatment: true, item, rowType, title});
+        imgTextTreatment.classList.add('text-treatment')
+        tile.appendChild(imgTextTreatment);
+    }
 
     return tile;
 }
 
-export function createTileImage({contentType, item, title}) {
+export function createTileImage({contentType, isTitleTreatment, item, rowType, title}) {
     const img = document.createElement('img');
-    const fallbackImage = `https://placehold.co/960x540?text=${encodeURI(title)}`;
+    let {aspectRatio, imageType} = getRowTypeConfig(rowType);
+    if (isTitleTreatment) {
+        imageType = 'title_treatment_layer';
+    }
+    const fallbackWidth = Math.round(540 * parseFloat(aspectRatio));
+    const fallbackImage = `https://placehold.co/${fallbackWidth}x540?text=${encodeURI(title)}`;
     img.src = fallbackImage;
-    if (item.image.tile['1.78']) {
-        console.log({contentType, title, '1.78': item.image.tile['1.78']})
-        img.src = item.image.tile['1.78'][contentType]?.default?.url;
+    if (item.image[imageType]?.[aspectRatio]) {
+        console.log({contentType, title, aspectRatio: item.image[imageType][aspectRatio]})
+        let imgDefault = item.image[imageType][aspectRatio].default?.default?.url;
+        let imgSrc = item.image[imageType][aspectRatio][contentType]?.default?.url || imgDefault;
+        if (isTitleTreatment) {
+            imgSrc = imgSrc.replace('format=jpeg', 'format=png');
+        }
+        img.src = imgSrc;
+    } else {
+        console.log(`image not found for ${title}`, {contentType, title, aspectRatio: item.image[imageType][aspectRatio]})
     }
     img.alt = title;
-    img.onerror = () => {
+    img.onerror = (e) => {
+        console.warn(`image error for ${title}`, e, {url: img.src, contentType, title, aspectRatio: item.image[imageType][aspectRatio]})
         img.src = fallbackImage;
         img.onerror = null;
     };
     return img;
+}
+
+export function getRowTypeConfig(rowType) {
+    switch (rowType) {
+        case RowTypes.BILLBOARD:
+            return {
+                aspectRatio: '3.00',
+                imageType: 'hero_tile'
+            };
+        case RowTypes.COLLECTION:
+            return {
+                aspectRatio: '0.71',
+                imageType: 'tile'
+            };
+        case RowTypes.GRID:
+        default:
+            return {
+                aspectRatio: '1.78',
+                imageType: 'tile'
+            };
+    }
 }
